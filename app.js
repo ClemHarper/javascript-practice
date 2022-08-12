2daa598e360772b002d2af4ed20ecd05affa6af1
ClemHarper <Clem.Harper@gmail.com>
2022-08-12 14:34:25 -0400

    select only 3 items working

--- app.js
+++ app.js
@@ -6,19 +6,21 @@ const soda = document.getElementById('soda');
 
 //step 2 - add an event listener
 soda.addEventListener('click', () => {
+    if (!soda.classList.contains('picked') && numberOfItems === 3) return;
     console.log('clicking');
     //step 3 - adding the classname
     soda.classList.toggle('picked');
     if (soda.classList.contains('picked')) {
         numberOfItems = numberOfItems + 1;
     } else {
-        // if no -- decrement number o fitems
+        // if no -- decrement number of items
         numberOfItems = numberOfItems - 1;
     }
     items.textContent = numberOfItems;
 });
 
 cheese.addEventListener('click', () => {
+    if (!cheese.classList.contains('picked') && numberOfItems === 3) return;
     console.log('clicking');
     cheese.classList.toggle('picked');
     if (cheese.classList.contains('picked')) {
@@ -30,34 +32,37 @@ cheese.addEventListener('click', () => {
 });
 
 grapes.addEventListener('click', () => {
+    if (!grapes.classList.contains('picked') && numberOfItems === 3) return;
     console.log('clicking');
     grapes.classList.toggle('picked');
     if (grapes.classList.contains('picked')) {
-        numberOfItems = numberOfItems + 1;
+        numberOfItems++;
     } else {
-        numberOfItems = numberOfItems - 1;
+        numberOfItems--;
     }
     items.textContent = numberOfItems;
 });
 
 bread.addEventListener('click', () => {
+    if (!bread.classList.contains('picked') && numberOfItems === 3) return;
     console.log('clicking');
     bread.classList.toggle('picked');
     if (bread.classList.contains('picked')) {
-        numberOfItems = numberOfItems + 1;
+        numberOfItems++;
     } else {
-        numberOfItems = numberOfItems - 1;
+        numberOfItems--;
     }
     items.textContent = numberOfItems;
 });
 
 watermelon.addEventListener('click', () => {
+    if (!watermelon.classList.contains('picked') && numberOfItems === 3) return;
     console.log('clicking');
     watermelon.classList.toggle('picked');
     if (watermelon.classList.contains('picked')) {
-        numberOfItems = numberOfItems + 1;
+        numberOfItems++;
     } else {
-        numberOfItems = numberOfItems - 1;
+        numberOfItems--;
     }
     items.textContent = numberOfItems;
 });
